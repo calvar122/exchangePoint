d49eb6d562440dbc0bbb330e30567b6ac7893b6d
Cesar Alvarado <cesar.alvarado@iteso.mx>
2021-09-17 16:08:56 -0500

    cuenta de calvar122

---
+++ cuentaRegresiva/backcount.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define _MY_VAR_ 12
+
+int main(){
+
+	printf("Este programa es una cuenta sucesiva de 12\n");
+	for(int i=0; i<_MY_VAR_; i++){
+		printf("Imprimiendo valor %d \n",i+1);
+	}
+return 0;
+
+}
